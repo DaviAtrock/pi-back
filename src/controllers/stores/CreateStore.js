17443a841082911bdb06290ac5b741c2bd105e59
Guilherme Braga <guilherme_braga100@hotmail.com>
2024-05-18 21:04:03 -0300

    fix: Adicionado validação para quando não é enviado o token.

--- src/controllers/stores/CreateStore.js
+++ src/controllers/stores/CreateStore.js
@@ -2,7 +2,14 @@ import { Models } from '../../models/index.js';
 
 export const CreateStore = async (req, res) => {
 
-    const setToken = req?.headers?.authorization || 'none';
+    const setToken = req?.headers?.authorization;
+
+    let responseModelValidation = await Models.validations.FieldsValidation({ type: 'token', params: { token: setToken }});
+
+    if(!responseModelValidation.status){
+        return res.status(401).json({ status: "error", description: responseModelValidation?.ValidationErrors?.token });
+    }
+
     const token = setToken.split(' ')[1];
 
     const responseModelSelectUserToken = await Models.database.SelectUserToken({ token: token });
@@ -11,7 +18,7 @@ export const CreateStore = async (req, res) => {
         return res.status(401).json({ status: "error", description: responseModelSelectUserToken?.errorMessage });
     }
 
-    const responseModelValidation = await Models.validations.FieldsValidation({ type: 'createStore', params: req.body });
+    responseModelValidation = await Models.validations.FieldsValidation({ type: 'createStore', params: req.body });
 
     if (!responseModelValidation.status) {
         return res.status(400).json({ status: "error", description: responseModelValidation?.ValidationErrors });
