4a6b957e9c7d969e1091cd44078ab8b56014e06b
Guilherme Braga <guilherme_braga100@hotmail.com>
2024-05-28 20:45:19 -0300

    fix: Ajustes de campos da requisição post /stores.

--- src/controllers/stores/CreateStore.js
+++ src/controllers/stores/CreateStore.js
@@ -18,7 +18,7 @@ export const CreateStore = async (req, res) => {
         return res.status(401).json({ status: "error", description: responseModelSelectUserToken?.errorMessage });
     }
 
-    responseModelValidation = await Models.validations.FieldsValidation({ type: 'createStore', params: req.body });
+    const responseModelValidation = await Models.validations.FieldsValidation({ type: 'createStore', params: req.body });
 
     if (!responseModelValidation.status) {
         return res.status(400).json({ status: "error", description: responseModelValidation?.ValidationErrors });
